2776173e2f2f99beceebf3ea3767d72735bc36d5
jpribyl <pribylsnbits@gmail.com>
2018-03-18 17:31:29 -0600

    update setting files, add template, integrate bootstrap

--- junebug/settings/production.py
+++ junebug/settings/production.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ux=0)efg6bf80+qa%1csdxnrnygzzux*q!z32fozuwe3-o$m=k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
--- junebug/urls.py
+++ junebug/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home')
 ]
+
+if settings.DEBUG:
+    urlpatterns += (static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT))
